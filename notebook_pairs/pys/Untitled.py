fb5f26da6f3fb67d643c22c31cda0d71a60490e1
dimitar popchev <dpopchev@gmail.com>
2024-09-15 13:37:16 +0300

    style:

--- notebook_pairs/pys/Untitled.py
+++ notebook_pairs/pys/Untitled.py
@@ -6,4 +6,17 @@
 #       format_name: percent
 #       format_version: '1.3'
 #       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
 # ---
+
+# %%
+from bgtorrent_observer.sample import sample
+
+# %%
+sample()
+
+# %%
+from bs
